8295b4178d7814c0ab9abafd03cb238d8297e0e5
David Palmero Jara <110841001+davidpalmerojara@users.noreply.github.com>
2024-10-04 22:59:53 +0200

    Added: lessons 19 and 20
    - Lesson 19: Environments and Configuration Flexibility
    
    - Lesson 20: SQL Injection Vulnerabilities Explained

--- Database.php
+++ Database.php
@@ -2,19 +2,22 @@
 
     class Database
     {
-        public $connection;
+        public PDO $connection;
 
-        public function __construct()
+        public function __construct($config, $username = 'root', $password = '')
         {
-            $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-            $this->connection = new PDO($dsn);
+            $dsn = 'mysql:host' . http_build_query($config, '', ';');
+
+            $this->connection = new PDO($dsn, $username, $password, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
         }
 
-        public function query($query)
+        public function query($query, $params = []):    PDOStatement
         {
             $statement = $this->connection->prepare($query);
 
-            $statement->execute();
+            $statement->execute($params);
 
             return $statement;
         }
--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <?php
-
     require "functions.php";
 
     require "Database.php";
-    //require "router.php";
+    // require "router.php";
 
     // Connect to the MySQL database and execute a query
 
-    $db = new Database();
+    $config = require "config.php";
+    $db = new Database($config['database']);
+    $id = null;
 
-    $posts = $db->query("select * from posts where id > 1")->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($posts as $post) {
-        echo "<li>" . $post['title'] . "</li>";
+    // Check if an ID parameter is provided in the URL query string
+    if (count($_GET) > 0) {
+        $id = $_GET['id'];
     }
+
+    // Fetch posts from the database based on the provided ID or all posts
+    if ($id) {
+        $query = "select * FROM posts WHERE id = :id";
+        $posts = $db->query($query, [':id' => $id])->fetch();
+
+        echo "<li>" . $posts['title'] . "</li>";
+    } else {
+        $query = "select * FROM posts";
+        $posts = $db->query($query)->fetchAll();
+
+        foreach ($posts as $post) {
+            echo "<li>" . $post['title'] . "</li>";
+        }
+    }
+
 
