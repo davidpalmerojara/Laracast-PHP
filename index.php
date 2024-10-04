8b774be1ac7b672150c9a2447f47831f30d07f51
davidpalmerojara <davidpalmerojara@gmail.com>
2024-10-04 14:17:25 +0200

    Added lessons 16, 17 and 18:
    - Lesson 16: Create a MySQL Database
    
    - Lesson 17: PDO First Steps
    
    - Lesson 18: Extract a PHP Database Class

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
     require "functions.php";
 
-    require "router.php";
+    require "Database.php";
+    //require "router.php";
+
+    // Connect to the MySQL database and execute a query
+
+    $db = new Database();
+
+    $posts = $db->query("select * from posts where id > 1")->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($posts as $post) {
+        echo "<li>" . $post['title'] . "</li>";
+    }
+
